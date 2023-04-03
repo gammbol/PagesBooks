47800591fb79de3178d52dd23d1dc8a889c201d7
gammbol <71922180+gammbol@users.noreply.github.com>
2023-04-03 15:27:26 +0300

    finished header and hero section

---
+++ scripts/mobile-menu.js
@@ -0,0 +1,20 @@
+let isOpened = false;
+const mobileMenuBtn = document.querySelector('.navbar__open');
+const mobileMenu = document.querySelector('.header__mobile-menu');
+const menuCartWrapper = document.querySelector('.mobile-menu__item:nth-of-type(6)');
+const menuCart = document.querySelector('.menu__cart');
+
+mobileMenuBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	isOpened = !isOpened
+	mobileMenu.style.top = isOpened ? '70px' : '-1000px';
+	document.body.style.overflow = isOpened ? 'hidden' : 'auto';
+});
+
+menuCartWrapper.addEventListener('mouseover', () => {
+	menuCart.src = "/images/carts/mobile-menu__cart-hover.svg";
+});
+
+menuCartWrapper.addEventListener('mouseout', () => {
+	menuCart.src = "/images/carts/navbar-cart.svg";
+});
